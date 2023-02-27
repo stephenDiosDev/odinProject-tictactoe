2082752f395b4ace1bc859b3c104697306273352
StephenDiosDev <stephendiosdeveloper@gmail.com>
2023-02-27 15:50:30 -0500

    I've broken it
    Review modules!

--- script.js
+++ script.js
@@ -16,16 +16,16 @@ const gameboard = (() => {
         htmlboard.childNodes.forEach(cell => {
             if(cell.className == "grid-cell") {
                 if(board[i] == 1) {
-                    cell.innerHTML = '<span class="material-icons-outlined md-80">close</span>';
+                    // cell.innerHTML = '<span class="material-icons-outlined md-80">close</span>';
+                    cell.firstChild.textContent = "close";
                 }
                 else if(board[i] == 2) {
-                    cell.innerHTML = '<span class="material-icons-outlined md-80">circle</span>';
-                }
-                else if(board[i] == 0) {
-                    cell.innerHTML = '<span class="material-icons-outlined md-80"></span>';
+                    // cell.innerHTML = '<span class="material-icons-outlined md-80">circle</span>';
+                    cell.firstChild.textContent = "circle";
                 }
                 else {
-                    cell.innerHTML = '<span class="material-icons-outlined md-80"></span>';
+                    // cell.innerHTML = '<span class="material-icons-outlined md-80"></span>';
+                    cell.firstChild.textContent = "test";
                 }
                 i++;
             }
@@ -63,18 +63,50 @@ const gameController = ((player1, player2) => {
     const activePlayer = null;
     const board = null;
 
-    const players = [player1, player2];
+    const players = [];
+
+    const addPlayers = (player1, player2) => {
+        players[0] = player1;
+        players[1] = player2;
+    }
 
     const processTurn = () => {
 
     }
 
     const renderBoard = () => {
-
+        // gameboard.renderBoard();
     }
 
     const checkForWinner = () => {
 
+    }
+
+    const setupBoard = () => {
+        let htmlboard = document.querySelector("#gameboard");
+
+        // add cells
+        for(let i = 0; i < 9; i++) {
+            let cell = document.createElement("div");
+            cell.setAttribute("id", "grid-cell");
+
+            let cellContent = document.createElement("span");
+            cellContent.classList.add("material-icons-outlined");
+            cellContent.classList.add("md-80");
+            cellContent.textContent = "test";
+
+            cell.addEventListener("mousedown", function(e) {
+                /*
+                    If cell content is empty
+                        Set the id based on the id of the current player
+                */
+               console.log(e);
+            });
+
+            cell.appendChild(cellContent);
+            htmlboard.appendChild(cell);
+        }
+        gameboard.renderBoard();
     }
 
     const switchActivePlayer = () => {
@@ -92,6 +124,13 @@ const gameController = ((player1, player2) => {
 
     }
 
+    return {
+        addPlayers,
+        setupBoard,
+        renderBoard,
+        processTurn
+    }
+
 
 })();
 
@@ -99,5 +138,12 @@ const playerFactory = (name, id) => {
     return {name, id}
 };
 
+
+let player1 = playerFactory("player1", 1);
+let player2 = playerFactory("player2", 2);
+
+let game = gameController;
+game.addPlayers(player1, player2);
+game.setupBoard();
 console.log("About to render board");
 gameboard.renderBoard();
